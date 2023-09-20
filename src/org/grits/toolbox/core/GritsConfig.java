fd79469d86bfeb037665a1a38ce4fea2870dd159
Sena Arpinar <sena.arpinar@gmail.com>
2023-09-20 19:50:01 -0400

    changed version to 1.4.0

--- src/org/grits/toolbox/core/GritsConfig.java
+++ src/org/grits/toolbox/core/GritsConfig.java
@@ -1,6 +1,6 @@
 package org.grits.toolbox.core;
 
 public class GritsConfig {
-	public static final String VERSION = "1.3.0";
-	public static final String VERSION_NAME = "Second Cup";
+	public static final String VERSION = "1.4.0";
+	public static final String VERSION_NAME = "Noon";
 }
